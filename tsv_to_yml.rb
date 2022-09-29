7cea394a81bfe117b5013cd1b17da27706d09951
KaiYi Chu <s28238385@gmail.com>
2022-09-30 02:35:50 +0800

    tsv_to_yml first step complete

---
+++ tsv_to_yml.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'yaml'
+require 'csv'
+
+def print_yaml
+  file_hashed = read_tsv
+  puts file_hashed[0].to_yaml
+end
+
+def create_yml
+  file_hashed = read_tsv
+  File.open(ARGV[1], 'w') do |f|
+    f.puts file_hashed.to_yaml
+  end
+end
+
+def read_tsv
+  file_open = CSV.table(ARGV[0], col_sep: "\t")
+  file_hashed = file_open.map { |line| line.to_hash }
+  file_hashed.each do |hash|
+    hash.keys.each { |key| hash[key.to_s] = hash.delete(key) }
+    hash['student_id'] = hash['student_id'].to_s
+    hash['tech_experience'] += "\r"
+  end
+end
+
+ARGV[1].nil? ? print_yaml : create_yml
